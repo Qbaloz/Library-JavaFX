a526fac06f9968708c600143f1cced80e3356963
Jakub Wawrzyniak <qbaloz1991@gmail.com>
2016-02-23 15:44:12 +0100

    Added thread to get books and display in table

--- src/main/java/com/starterkit/javafxlibrary/controller/BookListControler.java
+++ src/main/java/com/starterkit/javafxlibrary/controller/BookListControler.java
@@ -8,46 +8,81 @@ import com.starterkit.javafxlibrary.dataprovider.BookProvider;
 import com.starterkit.javafxlibrary.dataprovider.data.BookTo;
 import com.starterkit.javafxlibrary.model.BookListModel;
 
+import javafx.application.Platform;
+import javafx.beans.property.ReadOnlyLongWrapper;
+import javafx.beans.property.ReadOnlyStringWrapper;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 
 public class BookListControler {
-    
+
 	@FXML
 	TextField titleField;
-	
+
 	@FXML
 	Button searchButton;
-	
+
 	@FXML
 	TableView<BookTo> resultTable;
-	
-	private static final Logger LOG = Logger.getLogger(BookListControler.class);
-	
-	private Collection<BookTo> books = new ArrayList<>();
-	
-	private BookProvider bookProvider = BookProvider.INSTANCE;
-	
-	private final BookListModel model = new BookListModel();
-	
-	
-	public BookListControler() {
-		
-	}
-	
+
 	@FXML
-    private void initialize() {
+	TableColumn<BookTo, Number> idColumn;
+
+	@FXML
+	TableColumn<BookTo, String> titleColumn;
+
+	private static final Logger LOG = Logger.getLogger(BookListControler.class);
+
+	private BookProvider bookProvider = BookProvider.INSTANCE;
+
+	private final BookListModel model = new BookListModel();
+
+	public BookListControler() {
+
+	}
+
+	@FXML
+	private void initialize() {
+
+		initializeResultTable();
 		titleField.textProperty().bindBidirectional(model.titleProperty());
 		resultTable.itemsProperty().bind(model.resultProperty());
-		searchButton.disableProperty().bind(titleField.textProperty().isEmpty());
-    }    
-    
+		
+		model.setTitle("");
+	}
+
+	private void initializeResultTable() {
+		idColumn.setCellValueFactory(cellData -> new ReadOnlyLongWrapper(cellData.getValue().getId()));
+		titleColumn.setCellValueFactory(cellData -> new ReadOnlyStringWrapper(cellData.getValue().getTitle()));
+	}
+
 	@FXML
 	public void searchButtonAction(ActionEvent event) {
 		LOG.debug("'Search' button clicked");
-		books = bookProvider.findBooks(titleField.getText());
+		Runnable backgroundTask = new Runnable() {
+
+			@Override
+			public void run() {
+				LOG.debug("backgroundTask.run() called");
+
+				Collection<BookTo> result = bookProvider.findBooks(model.getTitle());
+
+				Platform.runLater(new Runnable() {
+
+					@Override
+					public void run() {
+						LOG.debug("Platform.runLater(Runnable.run()) called");
+						model.setResult(new ArrayList<BookTo>(result));
+						resultTable.getSortOrder().clear();
+					}
+				});
+			}
+		};
+		new Thread(backgroundTask).start();
 	}
+	
 }
